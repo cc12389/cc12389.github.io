04ce215deefb5dd8b13704041d4e7c55593cf574
cc12389 <141701543+cc12389@users.noreply.github.com>
2024-02-15 23:54:00 +0800

    Update camera_js.js

--- camera_js.js
+++ camera_js.js
@@ -5,7 +5,7 @@ const
     cameraView = document.querySelector("#camera-view"),
     cameraDevice = document.querySelector("#camera-device"),
     photoDisplay = document.querySelector("#photo-display"),
-    takePhotoButton = document.querySelector("#take-photo-button"),
+    takePhotoButton = document.querySelector("#take-photo-button");
     frontCameraButton = document.querySelector("front-camera-button");
 
 function cameraStart(){
